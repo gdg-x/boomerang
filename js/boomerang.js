1699a5af26d2c6cb0e3ebb5cc1dbea2de604fb7d
Joshua Woodward <jwwoodward@ebay.com>
2013-07-25 12:18:50 -0700

    Created separate config.js

--- js/boomerang.js
+++ js/boomerang.js
@@ -8,24 +8,6 @@ var boomerang = angular.module('gdgBoomerang', ['ngSanitize','ui.bootstrap'])
              otherwise({ redirectTo: '/about' });
     });
 
-boomerang.factory('Config',function(){
-    return {
-        //modify these
-        'name'          : 'GDG Fresno',
-        'id'            : '114769570436363155784',
-        'google_api'    : 'enter_your_own',
-        'pwa_id'        : '5846413253595166705', //picasa web album id, must belong to google+ id above
-        'cover' : {
-            title : 'DevFest Fresno',
-                subtitle : 'The largest Google Developer Conference in the Central Valley',
-                button : {
-                    text : 'Register',
-                    url : 'https://devfestfresno.eventbrite.com'
-                }
-        }
-    }
-});
-
 boomerang.controller('MainControl', function($scope, Config) {
     $scope.chapter_name = Config.name;
     $scope.google_plus_link = 'https://plus.google.com/' + Config.id;
---
+++ js/config.js
@@ -0,0 +1,17 @@
+boomerang.factory('Config',function(){
+    return {
+        //modify these
+        'name'          : 'GDG Fresno',
+        'id'            : '114769570436363155784',
+        'google_api'    : 'AIzaSyDPjRfTjr-X-FB0jRSf06_7rHAvu9oQ3To',
+        'pwa_id'        : '5846413253595166705', //picasa web album id, must belong to google+ id above
+        'cover' : {
+            title : 'DevFest Fresno',
+            subtitle : 'The largest Google Developer Conference in the Central Valley',
+            button : {
+                text : 'Register',
+                url : 'https://devfestfresno.eventbrite.com'
+            }
+        }
+    }
+});
