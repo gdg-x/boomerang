9e4ea3aac2522a5148ccfc3e52cd6bbf00a2bc94
splaktar <splaktar@gmail.com>
2013-08-24 12:55:37 -0400

    Setup config for GDG Space Coast.

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 boomerang.factory('Config',function(){
     return {
         //modify these
-        'name'          : 'GDG Fresno',
-        'id'            : '114769570436363155784',
-        'google_api'    : 'AIzaSyDPjRfTjr-X-FB0jRSf06_7rHAvu9oQ3To',
-        'pwa_id'        : '5846413253595166705', //picasa web album id, must belong to google+ id above
+        'name'          : 'GDG Space Coast',
+        'id'            : '103959793061819610212',
+        'google_api'    : 'AIzaSyBoJOzKrUyT4ShFVWvMWvwpNP-jPnhgZJs',
+        'pwa_id'        : '5915725140705884785', //picasa web album id, must belong to google+ id above
         'cover' : {
             title : 'DevFest Fresno',
             subtitle : 'The largest Google Developer Conference in the Central Valley',
