8b8ccfed8df87e61b4ad5af221c69c71b5f526df
ghchinoy <ghchinoy@gmail.com>
2014-06-06 08:19:55 -0600

    use hub.gdgx.io

--- js/config.js
+++ js/config.js
@@ -1,16 +1,16 @@
 boomerang.factory('Config',function(){
     return {
         //modify these
-        'name'          : 'GDG Fresno',
-        'id'            : '114769570436363155784',
-        'google_api'    : 'AIzaSyDPjRfTjr-X-FB0jRSf06_7rHAvu9oQ3To',
-        'pwa_id'        : '5846413253595166705', //picasa web album id, must belong to google+ id above
+        'name'          : 'GDG Northern Colorado',
+        'id'            : '100148220005153084415',
+        'google_api'    : 'AIzaSyCS3ILgldb7Oe4rjyr6j5UUvLpeSygj2U0',
+        //'pwa_id'        : '5846413253595166705', //picasa web album id, must belong to google+ id above
         'cover' : {
-            title : 'DevFest Fresno',
-            subtitle : 'The largest Google Developer Conference in the Central Valley',
+            title : 'GDG Northern Colorado',
+            subtitle : 'A Google Developer Group for Northern Colorado',
             button : {
-                text : 'Register',
-                url : 'https://devfestfresno.eventbrite.com'
+                text : 'Google Developers',
+                url : 'https://developers.google.com/groups/chapter/100148220005153084415/'
             }
         }
     }
