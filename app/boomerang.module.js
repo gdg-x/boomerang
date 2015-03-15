ee8183b55700f5e8cf351e652c00e6ed959cf7e8
Michael Prentice <splaktar@gmail.com>
2015-03-15 13:02:59 -0400

    Added config and icons for Twitter, Facebook, Meetup, and Google Developers.

--- app/boomerang.module.js
+++ app/boomerang.module.js
@@ -4,7 +4,10 @@ boomerang.controller('MainController', function ($rootScope, Config, NavService)
     var mc = this;
     mc.chapter_name = Config.name;
     mc.google_plus_link = 'https://plus.google.com/' + Config.id;
-    mc.isNavCollapsed = true;
+    mc.gdg_link = 'https://developers.google.com/groups/chapter/' + Config.id + '/';
+    mc.twitter_link = Config.twitter ? 'https://twitter.com/' + Config.twitter: null;
+    mc.facebook_link = Config.facebook ? 'https://www.facebook.com/' + Config.facebook: null;
+    mc.meetup_link = Config.meetup ? 'http://www.meetup.com/' + Config.meetup: null;
     $rootScope.canonical = Config.domain;
 
     NavService.registerNavListener(function (tab) {
--- app/services/configService.js
+++ app/services/configService.js
@@ -6,6 +6,9 @@ boomerang.factory('Config', function () {
         'google_api'    : '<insert your API key here>',
         'pwa_id'        : '5915725140705884785', // Picasa Web Album id, must belong to Google+ id above
         'domain'        : 'http://www.gdgspacecoast.org',
+        'twitter'       : 'gdgspacecoast',
+        'facebook'      : 'gdgspacecoast',
+        'meetup'        : 'gdgspacecoast',
         'cover' : {
             title: 'GDG Events Worldwide',
             subtitle: 'Worldwide directory of developer events organized by tags and displayed on a map.',
