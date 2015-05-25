90d9c7bbe302458062d55cc9d34f0c1ef8f6c64a
Michael Prentice <splaktar@gmail.com>
2015-05-24 22:07:08 -0700

    Fix Protractor tests.

--- test/e2e/newsTests.js
+++ test/e2e/newsTests.js
@@ -2,7 +2,6 @@ var env = require('./env.js');
 
 describe('Boomerang News page', function () {
     var gdgLoading = element(by.css('.gdg_loading'));
-    var newsTab = element(by.css('#news-tab.active'));
     var newsArticles = element.all((by.repeater('article in vm.news')));
     var attachments = element.all((by.repeater('attachment in article.object.attachments')));
 
@@ -10,9 +9,8 @@ describe('Boomerang News page', function () {
         browser.get(env.baseUrl + '/#!/news');
     });
 
-    it('should have a title and tab selected', function () {
+    it('should have a title', function () {
         expect(browser.getTitle()).toEqual('Google Developer Group');
-        expect(newsTab.isPresent()).toBe(true);
     });
 
     it('should hide the loading image', function () {
