4ada7739138e5044a4c72014cb83316f6b7fe81f
Michael Prentice <splaktar@gmail.com>
2019-09-25 04:54:49 -0400

    fix(tests): add angular-messages

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function (config) {
             'bower_components/angular-mocks/angular-mocks.js',
             'bower_components/angular-route/angular-route.js',
             'bower_components/angular-sanitize/angular-sanitize.js',
+            'bower_components/angular-messages/angular-messages.js',
             'bower_components/angular-aria/angular-aria.js',
             'bower_components/angular-animate/angular-animate.js',
             'bower_components/angular-material/angular-material.js',
